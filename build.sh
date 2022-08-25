7719cc894a1d2d7d7456c106c2f2ec63f052948a
Vetrox <39677514+Vetrox@users.noreply.github.com>
2022-08-25 09:26:00 +0200

    change build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 
 mkdir -p ./build
-g++ -g -std=c++2a src/numutils.cpp src/certparser.cpp src/tls.cpp src/http.cpp src/test.cpp -lcrypto -o build/a.out && ./build/a.out
+g++ -g -std=c++2a src/numutils.cpp src/certparser.cpp src/tls.cpp src/http.cpp src/test.cpp -O3 -o build/a.out && ./build/a.out
 
